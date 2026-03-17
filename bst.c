0f69eb11bdeada83082f0540518eb4e04db49403
Jahnvi Singh <f20240013@pilani.bits-pilani.ac.in>
2026-03-17 16:53:11 +0530

    Update bst.c

--- bst.c
+++ bst.c
@@ -5,11 +5,14 @@
 #include <limits.h>
 
 
+
+
 typedef struct node {
     int value;
     struct node *left;
     struct node *right;
 } Node;
+
 
 typedef struct bst {
     Node *root;
@@ -115,31 +118,33 @@ int find_max(BST *bst)
     return current->value;
 }
 
-Node *predecessor(Node *node)
+Node *predecessor(Node *node) // predeccessor : largest node smaller than the given node
 {
-    if (node->left == NULL)
+    if (node->left == NULL) // there is no node smaller than this node
     {
         return NULL;
     }
-    Node *current = node->left;
+    Node *current = node->left; // the left subtree has all nodes smaller than the current node
+    // the left child helps access all the smaller nodes
     while (current->right != NULL)
     {
         current = current->right;
-    }
+    } // traverse to the leaf of the right subtree of the left child of the given node to find the 
+    // largest node smaller than the given node
     return current;
 }
 
-Node *successor(Node *node)
+Node *successor(Node *node) // successor: smallest number greater than the node
 {
-    if (node->right == NULL)
+    if (node->right == NULL) // there is no node higher than this node
     {
         return NULL;
     }
-    Node *current = node->right;
-    while (current->left != NULL)
+    Node *current = node->right; // start to traverse the right child of the node(this or its children are successors)
+    while (current->left != NULL) 
     {
         current = current->left;
-    }
+    } // smallest successor is the leaf node of the left subtree of the right child of the node
     return current;
 }
 
@@ -147,10 +152,19 @@ void delete(BST *bst, Node *node)
 {
     if (node == NULL)
         return;
-    if (node->left == NULL && node->right == NULL)
+
+    if (node->left == NULL && node->right == NULL) // Case: Leaf node
     {
         // Node is a leaf
         Node* current = bst->root;
+
+        // if the node is a leaf, traverse to its parent, then the node can be either the left
+        // or right child of this parent node, just make the parent point to null to remove the node
+        // if the current node is not the parent of the node to be removed, check if the node can be 
+        // found in the left subtree or the right subtree 
+        // for this compare the value contained in the node with its left and right children and move the 
+        // current pointer accordingly
+
         while (current != NULL)
         {
             if (current->left == node)
@@ -176,19 +190,24 @@ void delete(BST *bst, Node *node)
         return;
     }
     
-    if (node->left == NULL)
+    if (node->left == NULL)  // Node only has right child
     {
-        // Node only has right child
+       
         Node* current = bst->root;
-        if (current == node)
+        if (current == node) // if the node to be deleted is the root node, and it has only the right child
+            // the root is updated to be the right child
         {
             bst->root = node->right;
             free(node);
             return;
         }
+
+
+        // otherwise : non leaf, non root node
         while (current != NULL)
         {
-            if (current->left == node)
+            if (current->left == node) // if left child of current node is to be deleted, point the left child
+                // to the right child of the node to be deleted(since we know that in has only the right child)
             {
                 current->left = node->right;
                 break;
@@ -198,6 +217,9 @@ void delete(BST *bst, Node *node)
                 current->right = node->right;
                 break;
             }
+
+            // if the node to be deleted is not the child of the current node then shift to the 
+            // left or right child node accordingly
             if (node->value < current->value)
             {
                 current = current->left;
@@ -211,11 +233,11 @@ void delete(BST *bst, Node *node)
         return;
     }
 
-    if (node->right == NULL)
+    if (node->right == NULL) // Node only has left child
     {
-        // Node only has left child
+        
         Node* current = bst->root;
-        if (current == node)
+        if (current == node) // node to be deleted is the root
         {
             bst->root = node->left;
             free(node);
@@ -223,6 +245,8 @@ void delete(BST *bst, Node *node)
         }
         while (current != NULL)
         {
+            //node to be deleted is the left or right child of some node
+
             if (current->left == node)
             {
                 current->left = node->left;
@@ -323,6 +347,51 @@ BST* constructBST(int A[], int n)
     return myBST;
 }
 
+
+
+
+
+/*
+void removeHalfNodes(BST *bst) //Doubt
+{
+    Node *current = bst->root;
+    if(bst == NULL)
+        return;
+
+    // check if current node is a half node
+
+    if(current->left != NULL && current->right == NULL) // only left child
+    {
+        Node *temp = current->parent;
+        temp->left = current->left;
+        free(current);
+        return;
+    }
+
+    if(current->right != NULL && current->left == NULL) // only right child
+    {
+        Node *temp = current->parent;
+        temp->left = current->left;
+        free(current);
+        return;
+    
+
+    
+
+
+    // identify nodes having either only left or right child
+    // remove them
+    //maintain BST property                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+
+}*/
+
+
+
+
+
+
+
+
 int checkBstHelper(Node *node,int min, int max)
 {
     if(node == NULL)
@@ -348,13 +417,7 @@ int checkBSTRec(BST *bst)
     return checkBstHelper(bst->root, INT_MIN, INT_MAX);
 }
 
-/*int checkBSTIte(BST *bst)
-{
-    if(bst == NULL)
-        return 1;
 
-
-}*/
 
 int max(int a, int b)
 {
@@ -375,7 +438,132 @@ int height(Node *node)
     
 
 }
+/*
+void removeHalfNode(BST *bst)
+{
+    Node* current = bst->root;
 
+    // checking left subtree of root
+    while(current != NULL)
+    { 
+    
+        
+        // delete that node while maintaining BST property
+
+        //halfnode is root
+        if(current == bst->root)
+        {
+            if(current->left !=NULL && current->right==NULL)
+            {
+                bst->root = current->left;
+                free(current);
+                
+            }
+           if(current->left ==NULL && current->right!=NULL)
+            {
+                bst->root = current->right;
+                free(current);
+                
+            }
+        }
+
+        
+        else // non root half nodes
+        {
+        if(current->value < current->parent->value) // current node is left node
+        {
+            if(current->left !=NULL && current->right==NULL) // current node has only left child
+            {
+                current->parent->left = current->left;
+
+            }
+            if(current->left ==NULL && current->right!=NULL) // current node has only right child
+            {
+                current->parent->left = current->right;
+            }
+        }
+
+        else // current node is right node
+        {
+            if(current->left !=NULL && current->right==NULL) // current node has only left child
+            {
+                current->parent->right = current->left;
+
+            }
+            if(current->left ==NULL && current->right!=NULL) // current node has only right child
+            {
+                current->parent->right = current->right;
+            }
+        }
+
+         }
+
+         current = current->left;
+    }
+
+     while(current != NULL)
+    { 
+    
+        
+        // delete that node while maintaining BST property
+
+        //halfnode is root
+        if(current == bst->root)
+        {
+            if(current->left !=NULL && current->right==NULL)
+            {
+                bst->root = current->left;
+                free(current);
+                
+            }
+           if(current->left ==NULL && current->right!=NULL)
+            {
+                bst->root = current->right;
+                free(current);
+                
+            }
+        }
+
+        
+        else // non root half nodes
+        {
+        if(current->value < current->parent->value) // current node is left node
+        {
+            if(current->left !=NULL && current->right==NULL) // current node has only left child
+            {
+                current->parent->left = current->left;
+
+            }
+            if(current->left ==NULL && current->right!=NULL) // current node has only right child
+            {
+                current->parent->left = current->right;
+            }
+        }
+
+        else // current node is right node
+        {
+            if(current->left !=NULL && current->right==NULL) // current node has only left child
+            {
+                current->parent->right = current->left;
+
+            }
+            if(current->left ==NULL && current->right!=NULL) // current node has only right child
+            {
+                current->parent->right = current->right;
+            }
+        }
+
+         }
+
+         current = current->right;
+    }
+
+
+
+
+}
+
+*/
 
 
 
@@ -395,27 +583,37 @@ int main()
     insert(bst, 6);
     insert(bst, 7);
     insert(bst, 8);
-   // traverse_in_order(bst->root);
-   //printf("\n");
-    //traverse_pre_order(bst->root);
-    //printf("\n");
-    //traverse_post_order(bst->root);
-    //printf("\n");
 
-    /*in_order_null(bst->root);
+    printf("In-order\n");
+   traverse_in_order(bst->root);
+   printf("\n");
+   printf("Pre-order\n");
+    traverse_pre_order(bst->root);
+    printf("\n");
+    printf("Post-order\n");
+    traverse_post_order(bst->root);
+    printf("\n");
+
+printf("in-order-null\n");
+    in_order_null(bst->root);
      printf("\n");
+     printf("Pre-order-null\n");
      pre_order_null(bst->root);
      printf("\n");
+     printf("Post-order-null\n");
      post_order_null(bst->root);
-     printf("\n");*/
+     printf("\n");
 
     int A[] = {1,2,3,4,5,6,7,8,};
     int k =8;
     BST *myBST = constructBST(A,k);
-    //traverse_in_order(myBST->root);
-    //printf("\n");
-    //traverse_pre_order(bst->root);
-    //printf("\n");
+
+    printf("constructed BST in-order\n");
+    traverse_in_order(myBST->root);
+    printf("\n");
+    printf("constructed BST pre-order\n");
+    traverse_pre_order(bst->root);
+    printf("\n");
 
     FILE* fp = fopen("n_integers.txt","r");
     int n;
@@ -443,8 +641,9 @@ int main()
 
     BST *bst1 = constructBST(arr,n);
 
-    traverse_in_order(bst1->root);
-    printf("\n");
+
+//   traverse_in_order(bst1->root);
+  //  printf("\n");
 
 
     if(checkBSTRec(myBST) == 1)
@@ -453,8 +652,9 @@ int main()
         printf("IS NOT BST \n");
 
 
-    printf("Height : %d ",height(bst1->root));
+    printf("Height : %d \n",height(bst1->root));
 
+   
 
 
     /*
